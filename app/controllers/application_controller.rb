7e9003dd32438e6fad1f3e80ae61f304bab02745
Marian <marianhalliday@gmail.com>
2016-08-29 22:23:20 -0400

    Making url helpers https in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # making url helpers https in production
+  def default_url_options(options = {})
+    if Rails.env.production?
+      { protocol: :https }.merge(options)
+    else
+      options
+    end
+  end
 end
