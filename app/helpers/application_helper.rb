7cb5554197569ac6b911c17927079a54dc686514
Marian <marianhalliday@gmail.com>
2016-09-01 22:03:34 -0400

    Adding SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def display_title
+    page_title = content_for?(:title) ? content_for(:title) : 'Blog'
+    "#{page_title} | Marian Serna"
+  end
+
+  def title(text)
+    content_for(:title) do
+      text
+    end
+  end
 end
